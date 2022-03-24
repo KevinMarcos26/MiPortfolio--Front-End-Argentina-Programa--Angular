2750ffc6d4fbdc5e918fc0e63f5f2a0a1a2487ec
KevinMarcos26 <kevinmarcos_96@yahoo.com.ar>
2022-03-24 13:12:37 -0300

    footer

---
+++ MiPortfolio_KevinAguiManera/src/app/components/hysk/hysk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hysk',
+  templateUrl: './hysk.component.html',
+  styleUrls: ['./hysk.component.css']
+})
+export class HyskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
